ab725171beca528f1d1e23c261e13a9b57194697
jackS <jastajas@gamil.com>
2018-02-06 21:02:38 +0100

    Klasa Person - ver.1

---
+++ src/Person.java
@@ -0,0 +1,40 @@
+public class Person {
+    private String  firstName;
+    private String lastName;
+    private int age;
+    private int pesel;
+
+    public Person(String firstName, String lastName, int age, int pesel) {
+
+        if (firstName == null || lastName == null || firstName.length() < 2 || lastName.length() < 2){
+            throw new NameUndefinedException();
+        } else {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        if (age < 1){
+            throw new IncorrectAgeException();
+        } else {
+            this.age = age;
+        }
+
+        String s = String.valueOf(pesel);
+        if (pesel < 1 || s.length() != 8){
+            throw new IncorrectPeselException();
+        } else {
+        this.pesel = pesel;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", pesel=" + pesel +
+                '}';
+    }
+}
